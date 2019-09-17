1accdebab3a2bad0deb557a2f8eb5d3a875141cf
Tianhang Zhang <tianhang.zhang@email.ucr.edu>
2019-09-17 10:41:00 -0400

    modify the service

---
+++ JooleWeb.SEV/ProductDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JooleWeb.Repo;
+using System.Data.Entity;
+using JooleWeb.DAL;
+
+namespace JooleWeb.SEV
+{
+    public partial class Service
+    {
+        public Product getProductObj(int id)
+        {
+            Product product = uow.product.GetByID(id);
+            return product;
+
+        }
+        public string getProductPrice(int id)
+        {
+            return uow.product.GetByID(id).Price.ToString();
+        }
+        public string getProductDescription(int id)
+        {
+            return uow.product.GetByID(id).Description.ToString();
+        }
+    }
+}
--- JooleWeb/Controllers/HomeController.cs
+++ JooleWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//using JooleWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -26,10 +27,39 @@ namespace JooleWeb.Controllers
 
             return View();
         }
-        public ActionResult login()
+        /*public ActionResult login()
         {
-            return View("login");
+            User user = new User();
+            //user.UserName = "Paul changed";
+            return View("login",user);
+           
         }
-            
+        [HttpPost]*/
+        /*public ActionResult Authorize(JooleWeb.Models.User userModel)
+        {
+            using (Joole_RBBTEntities db=new Joole_RBBTEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if(userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong user name or password";
+                    return View("login", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    Session["userName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }*/
+        public ActionResult logOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("login", "HomeController");
+
+        }
+
     }
 }
