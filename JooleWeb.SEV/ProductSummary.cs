4550dfe785bbf9952a04a2d102d8b68651b96e61
Robert Diamond <robertddiamond@gmail.com>
2019-09-18 11:24:11 -0400

    finished Summary Page

--- JooleWeb.SEV/ProductSummary.cs
+++ JooleWeb.SEV/ProductSummary.cs
@@ -30,9 +30,152 @@ namespace JooleWeb.SEV
             return productlist;
         }
 
-            
+
+        public List<Product> ElecSummary()
+        {
+            List<Product> products = new List<Product>();
+            products = uow.product.GetAll().ToList();
+            List<Product> productlist = new List<Product>();
+            foreach (var item in products)
+            {
+                if(item.CategoryID == 1)
+                {
+                    Product prod = new Product();
+                    prod.ProductID = item.ProductID;
+                    prod.ProductsName = item.ProductsName;
+                    prod.CategoryID = item.CategoryID;
+                    prod.Price = item.Price;
+                    prod.SubcategoryID = item.SubcategoryID;
+                    prod.ProductImage = item.ProductImage;
+                    prod.Description = item.Description;
+                    productlist.Add(prod);
+                }
+               
+            }
+            return productlist;
+        }
 
 
+        public List<Product> FurnSummary()
+        {
+            List<Product> products = new List<Product>();
+            products = uow.product.GetAll().ToList();
+            List<Product> productlist = new List<Product>();
+            foreach (var item in products)
+            {
+                if (item.CategoryID == 2)
+                {
+                    Product prod = new Product();
+                    prod.ProductID = item.ProductID;
+                    prod.ProductsName = item.ProductsName;
+                    prod.CategoryID = item.CategoryID;
+                    prod.Price = item.Price;
+                    prod.SubcategoryID = item.SubcategoryID;
+                    prod.ProductImage = item.ProductImage;
+                    prod.Description = item.Description;
+                    productlist.Add(prod);
+                }
+
+            }
+            return productlist;
+        }
+
+        public List<Product> TVSummary()
+        {
+            List<Product> products = new List<Product>();
+            products = uow.product.GetAll().ToList();
+            List<Product> productlist = new List<Product>();
+            foreach (var item in products)
+            {
+                if (item.CategoryID == 1 && item.SubcategoryID == 1)
+                {
+                    Product prod = new Product();
+                    prod.ProductID = item.ProductID;
+                    prod.ProductsName = item.ProductsName;
+                    prod.CategoryID = item.CategoryID;
+                    prod.Price = item.Price;
+                    prod.SubcategoryID = item.SubcategoryID;
+                    prod.ProductImage = item.ProductImage;
+                    prod.Description = item.Description;
+                    productlist.Add(prod);
+                }
+
+            }
+            return productlist;
+        }
+
+        public List<Product> HomeSummary()
+        {
+            List<Product> products = new List<Product>();
+            products = uow.product.GetAll().ToList();
+            List<Product> productlist = new List<Product>();
+            foreach (var item in products)
+            {
+                if (item.CategoryID == 1 && item.SubcategoryID == 2 )
+                {
+                    Product prod = new Product();
+                    prod.ProductID = item.ProductID;
+                    prod.ProductsName = item.ProductsName;
+                    prod.CategoryID = item.CategoryID;
+                    prod.Price = item.Price;
+                    prod.SubcategoryID = item.SubcategoryID;
+                    prod.ProductImage = item.ProductImage;
+                    prod.Description = item.Description;
+                    productlist.Add(prod);
+                }
+
+            }
+            return productlist;
+        }
+
+        public List<Product> TableSummary()
+        {
+            List<Product> products = new List<Product>();
+            products = uow.product.GetAll().ToList();
+            List<Product> productlist = new List<Product>();
+            foreach (var item in products)
+            {
+                if (item.CategoryID == 2 && item.SubcategoryID == 4)
+                {
+                    Product prod = new Product();
+                    prod.ProductID = item.ProductID;
+                    prod.ProductsName = item.ProductsName;
+                    prod.CategoryID = item.CategoryID;
+                    prod.Price = item.Price;
+                    prod.SubcategoryID = item.SubcategoryID;
+                    prod.ProductImage = item.ProductImage;
+                    prod.Description = item.Description;
+                    productlist.Add(prod);
+                }
+
+            }
+            return productlist;
+        }
+
+
+        public List<Product> ChairSummary()
+        {
+            List<Product> products = new List<Product>();
+            products = uow.product.GetAll().ToList();
+            List<Product> productlist = new List<Product>();
+            foreach (var item in products)
+            {
+                if (item.CategoryID == 2 && item.SubcategoryID == 3)
+                {
+                    Product prod = new Product();
+                    prod.ProductID = item.ProductID;
+                    prod.ProductsName = item.ProductsName;
+                    prod.CategoryID = item.CategoryID;
+                    prod.Price = item.Price;
+                    prod.SubcategoryID = item.SubcategoryID;
+                    prod.ProductImage = item.ProductImage;
+                    prod.Description = item.Description;
+                    productlist.Add(prod);
+                }
+
+            }
+            return productlist;
+        }
     }
 }
    
--- JooleWeb/Controllers/SummaryController.cs
+++ JooleWeb/Controllers/SummaryController.cs
@@ -11,28 +11,102 @@ namespace JooleWeb.Controllers
 {
     public class SummaryController : Controller
     {
+        JooleWeb.SEV.Service ProductSummary = new JooleWeb.SEV.Service();
+        List<Product> products;
         // GET: Summary
-        public ActionResult Summary()
+        public ActionResult Summary(string cat, string catSub)
         {
 
-            
 
-            JooleWeb.SEV.Service ProductSummary = new JooleWeb.SEV.Service();
-            List<Product> products = ProductSummary.AllSummary();
+            switch (cat)
+            {
+                case "1":
+                    switch(catSub)
+                    {
+                        case "1":
+                            products = ProductSummary.TVSummary();
+                            break;
+                        case "2":
+                            products = ProductSummary.HomeSummary();
+                            break;
+                        default:
+                            products = ProductSummary.ElecSummary();
+                            break;
+                    }           
+                    break;
+                case "2":
+                    switch (catSub)
+                    {
+                        case "3":
+                            products = ProductSummary.ChairSummary();
+                            break;
+                        case "4":
+                            products = ProductSummary.TableSummary();
+                            break;
+                        default:
+                            products = ProductSummary.FurnSummary();
+                            break;
+                    }
+                    break;
+
+                default:
+                    products = ProductSummary.AllSummary();
+                    break;
+
+            }
+
+
+           
+           
             var productList = new List<Summary>();
 
 
-            foreach(var item in products)
+            foreach (var item in products)
             {
                 var productItem = new Summary();
                 productItem.productName = item.ProductsName;
-                productItem.Category = item.CategoryID.ToString();
-                productItem.subcat = item.SubcategoryID.ToString();
+
+                switch ((item.CategoryID))
+                {
+                    case 1:
+                        productItem.Category = "Electronics";
+                        break;
+                    case 2:
+                        productItem.Category = "Furniture";
+                        break;
+
+                    default:
+                        productItem.Category = "Electronics";
+                        break;
+                }
+
+
+                switch ((item.SubcategoryID))
+                {
+                    case 1:
+                        productItem.subcat = "TV";
+                        break;
+
+                    case 2:
+                        productItem.subcat = "Home Theater";
+                        break;
+                    case 3:
+                        productItem.subcat = "Table";
+                        break;
+                    case 4:
+                        productItem.subcat = "Chair";
+                        break;
+                }
+
                 productItem.image = item.ProductImage;
                 productItem.price = item.Price.ToString();
                 productItem.desc = item.Description;
                 productList.Add(productItem);
+
             }
+
+
+
             return View(productList);
         }
     }
